1f12464e834cea57e1c4e4aaf6d7d893ef981e27
Wayne Warren <wayne.warren.s@gmail.com>
2019-10-15 14:57:38 +0000

    use Profile and Mounts aspects in chrome binary

--- app/chrome/src/main.rs
+++ app/chrome/src/main.rs
@@ -1,34 +1,11 @@
 use std::collections::HashMap;
 use std::env;
 
-use clap::ArgMatches;
 use tar::{Builder, Header};
 use tempfile::NamedTempFile;
 
 use dfiles::aspects;
 use dfiles::containermanager::new_container_manager;
-
-struct Chrome {}
-impl aspects::ContainerAspect for Chrome {
-    fn name(&self) -> String {
-        String::from("Chrome")
-    }
-    fn run_args(&self, _: Option<&ArgMatches>) -> Vec<String> {
-        let home = env::var("HOME").expect("HOME must be set");
-
-        vec![
-            "-v",
-            format!("{}/.config/google-chrome:/data", home).as_str(),
-            "-v",
-            format!("{}/downloads:/home/wayne/Downloads", home).as_str(),
-            "--name",
-            "chrome",
-        ]
-        .into_iter()
-        .map(String::from)
-        .collect()
-    }
-}
 
 fn main() {
     let tar_file = NamedTempFile::new().unwrap();
@@ -45,12 +22,18 @@ fn main() {
         a.append(&header, bs).unwrap();
     }
 
+    let home = env::var("HOME").expect("HOME must be set");
+    let host_path_prefix = format!("{}/.config/google-chrome", home);
+    let container_path = String::from("/data");
+
+    let host_downloads_path = format!("{}/downloads", home);
+    let container_downloads_path = format!("{}/Downloads", home);
+
     let mgr = new_container_manager(
         tar_file.path().to_path_buf(),
         vec![String::from("waynr/chrome:v0")],
         Vec::new(),
         vec![
-            Box::new(Chrome {}),
             Box::new(aspects::PulseAudio {}),
             Box::new(aspects::X11 {}),
             Box::new(aspects::Video {}),
@@ -60,6 +43,14 @@ fn main() {
             Box::new(aspects::Shm {}),
             Box::new(aspects::CPUShares("512".to_string())),
             Box::new(aspects::Memory("3072mb".to_string())),
+            Box::new(aspects::Profile {
+                host_path_prefix: host_path_prefix,
+                container_path: container_path,
+            }),
+            Box::new(aspects::Mounts(vec![aspects::Mount(
+                host_downloads_path,
+                container_downloads_path,
+            )])),
         ],
         vec!["google-chrome", "--user-data-dir=/data"]
             .into_iter()
