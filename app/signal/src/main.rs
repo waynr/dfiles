5fdfcf62371157677372c2586f896de703d3413a
Wayne Warren <wayne.warren.s@gmail.com>
2019-10-19 18:25:40 +0000

    add signal app

---
+++ app/signal/src/main.rs
@@ -0,0 +1,63 @@
+use std::collections::HashMap;
+use std::env;
+
+use tar::{Builder, Header};
+use tempfile::NamedTempFile;
+
+use dfiles::aspects;
+use dfiles::containermanager::new_container_manager;
+
+fn main() {
+    let tar_file = NamedTempFile::new().unwrap();
+    let mut a = Builder::new(&tar_file);
+
+    let mut context: HashMap<&str, &[u8]> = HashMap::new();
+    context.insert("Dockerfile", include_bytes!("Dockerfile"));
+    context.insert("pulse-client.conf", include_bytes!("pulse-client.conf"));
+    for (name, bs) in context {
+        let mut header = Header::new_gnu();
+        header.set_path(name).unwrap();
+        header.set_size(bs.len() as u64);
+        header.set_cksum();
+        a.append(&header, bs).unwrap();
+    }
+
+    let home = env::var("HOME").expect("HOME must be set");
+    let host_path_prefix = format!("{}/.config/signal/", home);
+    let container_path = format!("{}/.config/Signal/", home);
+
+    let host_downloads_path = format!("{}/downloads", home);
+    let container_downloads_path = format!("{}/Downloads", home);
+
+    let mgr = new_container_manager(
+        tar_file.path().to_path_buf(),
+        vec![String::from("waynr/signal:v0")],
+        Vec::new(),
+        vec![
+            Box::new(aspects::Name("signal".to_string())),
+            Box::new(aspects::PulseAudio {}),
+            Box::new(aspects::X11 {}),
+            Box::new(aspects::Video {}),
+            Box::new(aspects::DBus {}),
+            Box::new(aspects::NetHost {}),
+            Box::new(aspects::SysAdmin {}),
+            Box::new(aspects::Shm {}),
+            Box::new(aspects::CPUShares("512".to_string())),
+            Box::new(aspects::Memory("3072mb".to_string())),
+            Box::new(aspects::Profile {
+                host_path_prefix: host_path_prefix,
+                container_path: container_path,
+            }),
+            Box::new(aspects::Mounts(vec![aspects::Mount(
+                host_downloads_path,
+                container_downloads_path,
+            )])),
+        ],
+        vec!["/opt/Signal/signal-desktop"]
+            .into_iter()
+            .map(String::from)
+            .collect(),
+    );
+
+    mgr.execute("signal");
+}
