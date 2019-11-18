e25cde334d5c67de45c6cfa60b6b8fa80ccef9d5
Wayne Warren <wayne.warren.s@gmail.com>
2019-11-18 01:46:02 +0000

    skype: WIP, add initial setup

---
+++ app/skype/src/main.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+use std::env;
+
+use dfiles::aspects;
+use dfiles::containermanager::new_container_manager;
+use dfilesfiles::dfiles_files_container_mgr;
+
+fn main() {
+    let mut context: HashMap<String, String> = HashMap::new();
+    context.insert(
+        "Dockerfile".to_string(),
+        include_str!("skype.dockerfile").to_string(),
+    );
+
+    let home = env::var("HOME").expect("HOME must be set");
+    let host_path_prefix = format!("{}/.config/skypeforlinux", home);
+    let container_path = format!("{}/.config/skypeforlinux", home);
+
+    let host_downloads_path = format!("{}/downloads", home);
+    let container_downloads_path = format!("{}/Downloads", home);
+
+    let version = env!("CARGO_PKG_VERSION");
+
+    let dfilesfiles_mgr = dfiles_files_container_mgr();
+
+    let mgr = new_container_manager(
+        context,
+        vec![format!("{}:{}", "waynr/skype", version)],
+        vec![Box::new(dfilesfiles_mgr)],
+        vec![
+            Box::new(aspects::Name("skype".to_string())),
+            Box::new(aspects::PulseAudio {}),
+            Box::new(aspects::X11 {}),
+            Box::new(aspects::Video {}),
+            Box::new(aspects::DBus {}),
+            Box::new(aspects::NetHost {}),
+            Box::new(aspects::SysAdmin {}),
+            Box::new(aspects::Shm {}),
+            Box::new(aspects::CPUShares("512".to_string())),
+            Box::new(aspects::Memory("3072mb".to_string())),
+            Box::new(aspects::Profile {
+                host_path_prefix: host_path_prefix,
+                container_path: container_path,
+            }),
+            Box::new(aspects::Mounts(vec![aspects::Mount(
+                host_downloads_path,
+                container_downloads_path,
+            )])),
+        ],
+        vec!["skypeforlinux"]
+            .into_iter()
+            .map(String::from)
+            .collect(),
+    );
+
+    mgr.execute("skype");
+}
---
+++ app/skype/src/skype.dockerfile
@@ -0,0 +1,49 @@
+FROM dfilesfiles:0.1.0 as dfilesfiles 
+FROM debian/buster/wayne:0
+MAINTAINER Wayne Warren
+
+ADD https://go.skype.com/skypeforlinux-64.deb /var/skypeforlinux-64.deb
+
+RUN apt-get update && apt-get install -y \
+	dbus-x11 \
+	gnupg \
+	libpango1.0-0 \
+	hicolor-icon-theme \
+	libgl1-mesa-dri \
+	libgl1-mesa-glx \
+	libgtk2.0-0 \
+	libpulse0 \
+	pulseaudio \
+	libv4l-0 \
+	fonts-symbola \
+	--no-install-recommends \
+	&& apt-get purge --auto-remove -y curl \
+	&& rm -rf /var/lib/apt/lists/* \
+	&& rm -rf /src/*.deb
+
+RUN apt-get update && apt-get install -y \
+	libatk-bridge2.0-0 \
+	libatspi2.0-0 \
+	libgtk-3-0 \
+	libnspr4  \
+	libnss3 \
+	libsecret-1-0 \
+	libxss1 \
+	gnome-keyring \
+	--no-install-recommends \
+	&& apt-get purge --auto-remove -y curl \
+	&& rm -rf /var/lib/apt/lists/* \
+	&& rm -rf /src/*.deb
+
+RUN dpkg -i '/var/skypeforlinux-64.deb'
+
+
+COPY --from=dfilesfiles /pulse-client.conf /etc/pulse/client.conf
+
+USER wayne
+
+RUN mkdir -p /data
+# COPY local.conf /etc/fonts/local.conf
+
+# Autorun chrome
+CMD [ "google-chrome", "--user-data-dir=/data" ]
