f352057a80d4bf9802624fd89c1d0799123adc2c
Wayne Warren <wayne.warren.s@gmail.com>
2021-05-08 07:04:22 -0500

    apps/thinkorswim: make it actually work

--- app/thinkorswim/src/main.rs
+++ app/thinkorswim/src/main.rs
@@ -6,7 +6,9 @@ use dfiles::aspects;
 use dfiles::containermanager::ContainerManager;
 
 #[derive(Clone)]
-struct Thinkorswim {}
+struct Thinkorswim {
+    user_name: String,
+}
 
 impl aspects::ContainerAspect for Thinkorswim {
     fn name(&self) -> String {
@@ -20,7 +22,7 @@ impl aspects::ContainerAspect for Thinkorswim {
                 content: format!(
                     r#"
 RUN apt-get update && apt-get install -y --no-install-recommends \
-        openjdk-11-jre \
+        openjdk-8-jre \
     && rm -rf /var/lib/apt/lists/* \
     && rm -rf /src/*.deb"#,
                 ),
@@ -30,11 +32,32 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
                 content: format!(
                     r#"WORKDIR /opt/
 ADD https://mediaserver.thinkorswim.com/installer/InstFiles/thinkorswim_installer.sh ./
-RUN bash -x thinkorswim_installer.sh -q
+RUN chown -R {name}.{name} /opt/thinkorswim_installer.sh
+RUN chmod u+wrx /opt/thinkorswim_installer.sh
 "#,
+                    name = self.user_name,
+                ),
+            },
+            aspects::DockerfileSnippet {
+                order: 97,
+                content: format!(
+                    r#"
+COPY ./opt/run-thinkorswim.sh /run-thinkorswim.sh
+RUN chown -R {name}.{name} /run-thinkorswim.sh
+RUN chmod u+wrx /run-thinkorswim.sh
+ENTRYPOINT ["/run-thinkorswim.sh"]
+"#,
+                    name = self.user_name,
                 ),
             },
         ]
+    }
+
+    fn container_files(&self) -> Vec<aspects::ContainerFile> {
+        vec![aspects::ContainerFile {
+            container_path: String::from("./opt/run-thinkorswim.sh"),
+            contents: String::from(std::include_str!("entrypoint.sh")),
+        }]
     }
 }
 
@@ -43,6 +66,12 @@ fn main() -> Result<()> {
 
     let version = env!("CARGO_PKG_VERSION");
 
+    let current_user = aspects::CurrentUser::detect().context("detecting current user")?;
+    let tos = Thinkorswim {
+        user_name: current_user.name(),
+    };
+
+    let thinkorswim_install_dir = format!("{}/thinkorswim", home);
     let mut mgr = ContainerManager::default_debian(
         "thinkorswim".to_string(),
         vec![format!("{}:{}", "waynr/thinkorswim", version)],
@@ -50,25 +79,24 @@ fn main() -> Result<()> {
             format!("{}/.thinkorswim", home),
             format!("{}/.java", home),
             format!("{}/.install4j", home),
-            format!("{}/.com.devexperts.tos.ui.user.login.ThinkOrSwimApplication", home),
+            thinkorswim_install_dir.clone(),
+            format!(
+                "{}/.com.devexperts.tos.ui.user.login.ThinkOrSwimApplication",
+                home
+            ),
         ],
         vec![
-            Box::new(Thinkorswim {}),
+            Box::new(tos),
             Box::new(aspects::Name("thinkorswim".to_string())),
-            Box::new(aspects::CurrentUser::detect().context("detecting current user")?),
+            Box::new(current_user),
             Box::new(aspects::PulseAudio {}),
             Box::new(aspects::X11 {}),
             Box::new(aspects::Video {}),
             Box::new(aspects::DBus {}),
             Box::new(aspects::Shm {}),
         ],
-        vec![
-            "/opt/thinkorswim/thinkorswim",
-        ]
-        .into_iter()
-        .map(String::from)
-        .collect(),
-        Some(String::from("bullseye")),
+        vec![format!("{}/thinkorswim", thinkorswim_install_dir)],
+        Some(String::from("stretch")),
     );
 
     mgr.execute().context("executing thinkorswim in container")
