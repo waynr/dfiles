9e9cc813c855957bac69840528341a73bec409e7
wayne warren <wayne.warren.s@gmail.com>
2023-04-09 11:14:55 -0600

    docker: remove unused code & dependencies

--- src/dfiles/docker.rs
+++ src/dfiles/docker.rs
@@ -1,36 +1,5 @@
 use std::process::Command;
 
-use serde::Deserialize;
-use serde_json::from_value;
-use shiplift::BuildOptions;
-use shiplift::Docker;
-use tokio::prelude::Future;
-use tokio::prelude::Stream;
-
-#[derive(Deserialize, Debug)]
-struct BuildOutput {
-    stream: String,
-}
-
-pub fn build(opts: &BuildOptions) {
-    let docker = Docker::new();
-
-    let fut = docker
-        .images()
-        .build(opts)
-        .for_each(|output| {
-            let u: Result<BuildOutput, _> = from_value(output);
-            match u {
-                Ok(a) => print!("{}", a.stream),
-                Err(_) => (),
-            }
-            Ok(())
-        })
-        .map_err(|e| log::error!("Error: {}", e));
-
-    tokio::run(fut);
-}
-
 pub fn run(args: Vec<String>) {
     let cmdstr: String = args.join(" ");
     log::debug!("docker run {}", cmdstr);
