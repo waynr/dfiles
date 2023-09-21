572b3a857ce4b75291b029515a7f74015b4c4683
wayne warren <wayne.warren.s@gmail.com>
2023-09-20 21:42:56 -0600

    app/discord: update to version 0.0.30

--- app/discord/src/main.rs
+++ app/discord/src/main.rs
@@ -18,7 +18,7 @@ impl aspects::ContainerAspect for Discord {
             aspects::DockerfileSnippet {
                 order: 91,
                 content: r#"WORKDIR /opt/
-RUN curl https://dl.discordapp.net/apps/linux/0.0.25/discord-0.0.25.deb > /opt/discord.deb && \
+RUN curl https://dl.discordapp.net/apps/linux/0.0.30/discord-0.0.30.deb > /opt/discord.deb && \
     dpkg --force-depends -i /opt/discord.deb  ; rm /opt/discord.deb
 RUN apt-get update && apt-get --fix-broken install -y \
   && apt-get purge --autoremove \
