f7a78b2c1cad756e09612cadc2fe398d00827309
wayne warren <wayne.warren.s@gmail.com>
2024-01-30 11:34:59 -0700

    app/firefox: update firefox to 120.0

--- app/firefox/src/main.rs
+++ app/firefox/src/main.rs
@@ -8,7 +8,7 @@ use dfiles::containermanager::ContainerManager;
 #[derive(Clone)]
 struct Firefox {}
 
-const VERSION: &str = "119.0.1";
+const VERSION: &str = "121.0";
 
 impl aspects::ContainerAspect for Firefox {
     fn name(&self) -> String {
